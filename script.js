9f4fbcbf91aa0b228da1f3a503db48e526aab369
JOHN NASIMBA <nasimba4john@gmail.com>
2020-11-28 19:05:57 +0200

    created side nav

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const searchForm = document.getElementById('search-form');
 const searchIcon = document.getElementById('search-icon');
 const searchButton = document.getElementById('search-button');
 
+    // Create a media condition that targets view ports of screen width less than 500px
+
+const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+
 function displayForm() {
     searchIcon.hidden = true;
     headerTitle.hidden = true;
@@ -11,8 +16,31 @@ function displayForm() {
 function hideFormOnSearchButtonClick() {
     searchIcon.hidden = false;
     headerTitle.hidden = false;
-    searchForm.hidden = true;
+    if (mediaQuery.matches) {
+        searchForm.hidden = true
+    } else {
+        searchForm.hidden = false;
+      }
 }
 
 searchIcon.addEventListener('click', displayForm);
 searchButton.addEventListener('click', hideFormOnSearchButtonClick)
+
+// window.addEventListener('load', (event) => {
+
+//     // Check if the media query is true
+//     if (mediaQuery.matches) {
+//       searchForm.hidden = true
+//     }
+// });
+// function changeWhatToDisplay() {
+//     if (mediaQuery.matches) {
+//         // alert('the width is less than 500px');
+//         searchForm.hidden = true
+//       }
+// }
+// window.onresize = changeWhatToDisplay;
+
+
+// side nav scripts
+
